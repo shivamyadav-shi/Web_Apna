5158000d9a41955255214a4638d522ec7541cc07
shivam_shi <sy8115355575@gmail.com>
2026-03-17 23:55:29 +0530

    JS Start Template literals

---
+++ Apna_js/script.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+let a=5;
+let b=10;
+console.log("sum is", a+b);
+let pencilPrice=10;
+let eraser=5
+console.log("total price is"+ (pencilPrice+eraser) + " rupees");
+console.log(`your pay is ${pencilPrice + eraser}`);
+// arithmetic operation
+let num1 =10 ;
+let num2= 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(++a);
+console.log(--a);
+console.log(a--);
+console.log(18>17)
