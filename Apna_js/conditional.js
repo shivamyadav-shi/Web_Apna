032e711844752fc498117c65be2a937734c4b687
shivam_shi <sy8115355575@gmail.com>
2026-03-19 23:30:53 +0530

    Conditional Ststement

---
+++ Apna_js/conditional.js
@@ -0,0 +1,16 @@
+console.log("Before if conditon applied");
+let age = 21;
+if(age < 18){
+    console.log("You cannot vote");
+    console.log("You cannot drive");
+}
+if(age >=18){
+    console.log("You can vote");
+    console.log("You can drive");
+}
+console.log("After if conditon applied");
+
+let firstName = "DARSH";
+if(firstName=='DARSH'){
+    console.log(`Welcome ${firstName}`);
+}
---
+++ Apna_js/traffic.js
@@ -0,0 +1,11 @@
+// Traffic Signal
+let color="green"
+if (color=="red"){
+    console.log("Stop your vehicle");
+}
+if (color=="yellow"){
+    console.log("Ready to go");
+}
+if (color=="green"){
+    console.log("Go!Go!Go!");
+}
