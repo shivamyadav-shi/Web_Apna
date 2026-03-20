d5e0241c84792a09ed92a3efc5f5f9cf0c9484a9
shivam_shi <sy8115355575@gmail.com>
2026-03-21 00:17:10 +0530

    cond stmt: if-else if - else

---
+++ Apna_js/elseif.js
@@ -0,0 +1,19 @@
+let age = 23;
+if (age < 18){
+console.log("");
+} else if(age>=18){
+    console.log("You can vote");
+}
+
+// grade system
+let marks=81;
+if(marks>90){
+    console,log("Outstanding");
+}else if(marks >=60){
+    console.log("Grade 'A'");
+} else if(marks>40){
+    console.log("needs improvement");
+}
+else if(marks<33){
+    console.log("Try next year");
+}
---
+++ Apna_js/prQs.js
@@ -0,0 +1,16 @@
+let size="L";
+if(size ==='XL'){
+    console.log("Popcorn price-250 rupees");
+}
+else if(size ==='L'){
+    console.log("Popcorn price-200 rupees");
+}
+else if(size ==='M'){
+    console.log("Popcorn price-150 rupees");
+}
+else if(size ==='S'){
+    console.log("Popcorn price-100 rupees");
+}
+else{
+    console.log("Popcorn price-Not a valid size");
+}
