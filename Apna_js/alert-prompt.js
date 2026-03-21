b4fcc79d16609749fcb17a8cb657973ad2aced3d
shivam_shi <sy8115355575@gmail.com>
2026-03-22 01:41:04 +0530

    Js ch 2

---
+++ Apna_js/alert-prompt.js
@@ -0,0 +1,14 @@
+// alert("Something wrong");
+console.error("an error occured");
+console.warn("warning$$");
+let f_Name=prompt("enter yout name");
+console.log(f_Name);
+let Roll_No=prompt("enter yout roll no");
+console.log(Roll_No);
+let fi_Name=prompt("enter yout name");
+console.log(f_Name);
+let l_Name=prompt("enter yout surname");
+console.log(f_Name);
+let fullName = fi_Name + " "+l_Name;
+console.log(fullName);
+alert("Welcome"+fullName)
---
+++ Apna_js/switch.js
@@ -0,0 +1,40 @@
+let color = "yellow";
+switch(color){
+    case "red":
+        console.log("stop");
+        break;
+     case "yellow":
+        console.log("ready to go");
+        break;
+    case "green":
+        console.log("gogogo!");
+        break;
+    default:
+        console.log("not a valid traffic signal");
+}
+let day = 9;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Not a valid day");
+}
